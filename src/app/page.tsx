c7347b7fa1895c0df5f04412cc379f7de2f01b6e
Jona <llamasjonat@gmail.com>
2024-08-04 18:15:01 -0300

    fix(page): general changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,11 @@ export default function Home() {
   const [countTickets, setCountTickets] = useState<number>(1);
   const [screen, setScreen] = useState<string>('information');
 
+  // Form
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [newsletter, setNewsletter] = useState<boolean>(false);
+
   const [invoice, setInvoice] = useState<string>('');
 
   // Mock data
@@ -105,14 +110,14 @@ export default function Home() {
                         <path
                           d='M20 12L4 12'
                           stroke='currentColor'
-                          stroke-width='1.5'
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
+                          strokeWidth='1.5'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
                         />
                       </svg>
                     </Button>
                   )}
-                  <p className='flex items-center justify-center gap-2 w-[40px] font-semibold'>
+                  <p className='flex items-center justify-center gap-1 w-[40px] font-semibold'>
                     {screen !== 'information' && <span className='font-normal text-xs text-gray-500'>x</span>}
                     {countTickets}
                   </p>
@@ -134,9 +139,9 @@ export default function Home() {
                         <path
                           d='M12 4V20M20 12H4'
                           stroke='currentColor'
-                          stroke-width='1.5'
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
+                          strokeWidth='1.5'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
                         />
                       </svg>
                     </Button>
@@ -146,20 +151,6 @@ export default function Home() {
               {screen === 'information' && <p className='mt-2 text-sm text-gray-500'>{ticket.description}</p>}
             </Card>
             <div className='p-4'>
-              {screen === 'information' && (
-                <>
-                  <div className='flex gap-4 justify-between items-center'>
-                    <div className='flex gap-4 items-center'>
-                      <p className='text-gray-500'>Subtotal</p>
-                      <Button variant='link' size='sm'>
-                        Agregar cupon
-                      </Button>
-                    </div>
-                    <p className='font-bold text-md'>$10.000</p>
-                  </div>
-                  <hr className='my-4 border-gray-300' />
-                </>
-              )}
               <div className='flex gap-4 justify-between items-center'>
                 <p className='text-gray-500'>Total</p>
                 <p className='font-bold text-md'>
@@ -207,29 +198,52 @@ export default function Home() {
                 </div>
                 <Card className='p-6'>
                   <div className='flex flex-col flex-1 justify-start'>
-                    <form onSubmit={() => null} className='flex flex-col gap-4'>
-                      <div className='flex flex-col gap-2'>
+                    <form method='POS' onSubmit={handleGenerateInvoice} className='flex flex-col gap-4'>
+                      <div className='flex flex-col gap-4'>
                         <div className='flex flex-col gap-2'>
                           <Label htmlFor='name'>Name</Label>
-                          <Input type='text' id='name' name='name' placeholder='Satoshi Nakamoto' />
+                          <Input
+                            type='text'
+                            id='name'
+                            name='name'
+                            placeholder='Satoshi Nakamoto'
+                            required
+                            onChange={(e) => setName(e.target.value)}
+                            defaultValue={name}
+                          />
                         </div>
-                        {/* <div className='mt-4 mb-2'>
-                          <h3 className='font-bold'>Contact</h3>
-                        </div> */}
                         <div className='flex flex-col gap-2'>
                           <Label htmlFor='email'>Email</Label>
-                          <Input type='email' id='email' name='email' placeholder='satoshi@gmail.com' />
+                          <Input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='satoshi@gmail.com'
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                            defaultValue={email}
+                          />
                         </div>
-                        <div className='flex items-center gap-2 mt-2'>
-                          <Checkbox id='newsletter' checked />
-                          <Label htmlFor='newsletter'>Subscribe to the newsletter</Label>
+                        <div className='items-top flex space-x-2 mt-2'>
+                          <Checkbox id='newsletter' onChange={() => setNewsletter(!newsletter)} />
+                          <div className='grid gap-1.5 leading-none'>
+                            <label
+                              htmlFor='newsletter'
+                              className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                            >
+                              Subscribe to the newsletter
+                            </label>
+                            <p className='text-sm text-muted-foreground'>
+                              You agree to our Terms of Service and Privacy Policy.
+                            </p>
+                          </div>
                         </div>
                         {/* <div className='flex items-center gap-2 mt-1'>
                           <Checkbox id='terms' />
                           <Label htmlFor='terms'>Accept terms and conditions</Label>
                         </div> */}
                       </div>
-                      <Button onClick={handleGenerateInvoice}>Confirm order</Button>
+                      <Button type='submit'>Confirm order</Button>
                     </form>
                   </div>
                 </Card>
@@ -238,11 +252,12 @@ export default function Home() {
 
             {screen === 'payment' && (
               <div className='flex-1 flex flex-col gap-4'>
-                <div className='flex gap-4 items-center justify-between'>
-                  <div className='flex flex-col gap-2'>
+                <div className='flex gap-4 items-center justify-end'>
+                  {/* <div className='flex flex-col gap-2'>
                     <h2 className='font-bold text-2xl'>Payment</h2>
                     <p className='text-gray-500'>Lorem ipsum dolor sit, amet consectetur</p>
-                  </div>
+                  </div> */}
+                  <p className='text-sm text-gray-500'>Time: </p>
                   <div className='flex gap-2 items-center justify-center'>
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
@@ -338,24 +353,74 @@ export default function Home() {
 
             {screen === 'summary' && (
               <>
-                <div className='relative z-1 flex-1 flex flex-col gap-4'>
-                  <div className='flex flex-col gap-2'>
-                    <h2 className='font-bold text-2xl'>Summary</h2>
-                    <p className='text-gray-500'>Lorem ipsum dolor sit, amet consectetur</p>
+                <Card>
+                  <div className='flex-1 flex flex-col items-center justify-center gap-4 w-full mx-auto py-12 px-8'>
+                    <CreditCardValidationIcon className='w-8 h-8' />
+                    <div className='flex flex-col gap-2 text-center'>
+                      <h2 className='font-bold text-2xl'>Congratulation!</h2>
+                      <p className='text-gray-500'>
+                        Your payment has been confirmed. We have sent the event details to your email.
+                      </p>
+                    </div>
                   </div>
-                  <div className='flex flex-col flex-1 justify-start'>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe at provident autem fugit iste quo
-                      quae numquam quibusdam quam et nemo doloremque expedita debitis vitae reiciendis id veniam, neque
-                      sapiente.
-                    </p>
-                  </div>
-                </div>
+                </Card>
+                <Button className='w-full' variant='link'>
+                  Back to page
+                </Button>
               </>
             )}
           </div>
         </section>
       </div>
     </>
+  );
+}
+
+function CreditCardValidationIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      width='24'
+      height='24'
+      color='currentColor'
+      fill='none'
+    >
+      <path
+        d='M15 7.5C15 7.5 15.5 7.5 16 8.5C16 8.5 17.5882 6 19 5.5'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M22 7C22 9.76142 19.7614 12 17 12C14.2386 12 12 9.76142 12 7C12 4.23858 14.2386 2 17 2C19.7614 2 22 4.23858 22 7Z'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+      />
+      <path
+        d='M3.60746 21.0095L4.07229 20.4209L3.60746 21.0095ZM3.0528 20.4875L3.61262 19.9884L3.0528 20.4875ZM20.9472 20.4875L20.3874 19.9884L20.9472 20.4875ZM20.3925 21.0095L19.9277 20.4209L20.3925 21.0095ZM3.60746 6.99127L3.14263 6.40268L3.60746 6.99127ZM3.0528 7.5133L3.61262 8.0124L3.0528 7.5133ZM22.75 13.2445C22.7493 12.8302 22.4129 12.495 21.9987 12.4958C21.5845 12.4965 21.2493 12.8329 21.25 13.2471L22.75 13.2445ZM9.06582 6.75292C9.48003 6.75057 9.81391 6.41289 9.81157 5.99869C9.80922 5.58448 9.47154 5.2506 9.05734 5.25294L9.06582 6.75292ZM13.5 21.2504H10.5V22.7504H13.5V21.2504ZM10.5 21.2504C8.60311 21.2504 7.24353 21.2493 6.19895 21.1313C5.16816 21.0148 4.54359 20.7931 4.07229 20.4209L3.14263 21.5981C3.926 22.2168 4.86842 22.4905 6.03058 22.6218C7.17896 22.7515 8.63832 22.7504 10.5 22.7504V21.2504ZM1.25 14.0004C1.25 15.7493 1.24857 17.1321 1.38762 18.2226C1.52932 19.3337 1.82681 20.2394 2.49298 20.9866L3.61262 19.9884C3.22599 19.5547 2.99708 18.9856 2.87558 18.0328C2.75143 17.0593 2.75 15.789 2.75 14.0004H1.25ZM4.07229 20.4209C3.90545 20.2892 3.7517 20.1444 3.61262 19.9884L2.49298 20.9866C2.69068 21.2084 2.90811 21.4129 3.14263 21.5981L4.07229 20.4209ZM21.25 14.0004C21.25 15.789 21.2486 17.0593 21.1244 18.0328C21.0029 18.9856 20.774 19.5547 20.3874 19.9884L21.507 20.9866C22.1732 20.2394 22.4707 19.3337 22.6124 18.2226C22.7514 17.1321 22.75 15.7493 22.75 14.0004H21.25ZM13.5 22.7504C15.3617 22.7504 16.821 22.7515 17.9694 22.6218C19.1316 22.4905 20.074 22.2168 20.8574 21.5981L19.9277 20.4209C19.4564 20.7931 18.8318 21.0148 17.801 21.1313C16.7565 21.2493 15.3969 21.2504 13.5 21.2504V22.7504ZM20.3874 19.9884C20.2483 20.1444 20.0946 20.2892 19.9277 20.4209L20.8574 21.5981C21.0919 21.4129 21.3093 21.2084 21.507 20.9866L20.3874 19.9884ZM2.75 14.0004C2.75 12.2118 2.75143 10.9415 2.87558 9.96799C2.99708 9.01519 3.22599 8.44606 3.61262 8.0124L2.49298 7.0142C1.82681 7.76141 1.52932 8.66709 1.38762 9.77825C1.24857 10.8687 1.25 12.2515 1.25 14.0004H2.75ZM3.14263 6.40268C2.90811 6.58789 2.69068 6.79245 2.49298 7.0142L3.61262 8.0124C3.7517 7.8564 3.90545 7.71161 4.07229 7.57986L3.14263 6.40268ZM22.75 14.0004C22.75 13.7412 22.7504 13.4875 22.75 13.2445L21.25 13.2471C21.2504 13.4885 21.25 13.7376 21.25 14.0004H22.75ZM9.05734 5.25294C7.64978 5.26091 6.50411 5.29333 5.56558 5.44144C4.61301 5.59178 3.81862 5.86882 3.14263 6.40268L4.07229 7.57986C4.47956 7.25822 5.00124 7.04907 5.79942 6.92311C6.61164 6.79492 7.65139 6.76092 9.06582 6.75292L9.05734 5.25294Z'
+        fill='currentColor'
+      />
+      <path
+        d='M10 18H11.5'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        stroke-miterlimit='10'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M14.5 18L18 18'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        stroke-miterlimit='10'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path d='M2.5 11H10' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
   );
 }
