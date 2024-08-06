86ca83b30409e9c1064ca2ff8d442ec25bfb87c0
Jona <llamasjonat@gmail.com>
2024-08-06 11:28:46 -0300

    fix: structure for form-payment

--- src/app/component/form-payment.tsx
+++ src/app/component/form-payment.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 
 import { Button } from '@/components/ui/button';
@@ -10,57 +9,20 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Badge } from '@/components/ui/badge';
 
 export function FormPayment({ invoice }: any) {
-  const [countTickets, setCountTickets] = useState<number>(1);
-  const [screen, setScreen] = useState<string>('information');
-
-  // Mock data
-  const ticket = {
-    title: 'Ticket #1',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit impedit aperiam, optio dolores tenetur earum.',
-    imageUrl: 'https://placehold.co/400',
-    value: 5000,
-    valueType: 'SAT',
-  };
-
-  const handleGenerateInvoice = () => {
-    setScreen('payment');
-  };
-
   return (
     <>
       <div className='flex-1 flex flex-col gap-4'>
         <div className='flex gap-4 items-center justify-end'>
-          {/* <div className='flex flex-col gap-2'>
-                    <h2 className='font-bold text-2xl'>Payment</h2>
-                    <p className='text-gray-500'>Lorem ipsum dolor sit, amet consectetur</p>
-                  </div> */}
-          <p className='text-sm text-gray-500'>Time: </p>
+          <p className='text-sm text-text'>Time:</p>
           <div className='flex gap-2 items-center justify-center'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='20'
-              height='20'
-              color='bg-gray-50'
-              fill='none'
-            >
-              <circle cx='12' cy='12' r='10' stroke='currentColor' stroke-width='1.5' />
-              <path
-                d='M12 8V12L14 14'
-                stroke='currentColor'
-                stroke-width='1.5'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-              />
-            </svg>
-            <span className='font-semibold'>09:45</span>
+            <TimeIcon className='w-4 h-4' />
+            <span className='font-semibold text-sm'>09:45</span>
           </div>
         </div>
 
         <div className='flex-1 flex flex-col gap-4 justify-start'>
           <Tabs defaultValue='lightning'>
-            <TabsList className='w-full'>
+            <TabsList className='w-full bg-border'>
               <TabsTrigger value='lightning' className='flex-1'>
                 Lightning
               </TabsTrigger>
@@ -69,42 +31,35 @@ export function FormPayment({ invoice }: any) {
               </TabsTrigger>
             </TabsList>
             <TabsContent value='lightning'>
-              <Card>
-                <CardHeader>
-                  <CardTitle>Lightning Network</CardTitle>
-                  <CardDescription>Lorem ipsum dolor sit, amet consectetur</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <div className='max-w-[300px] mx-auto'>
-                    {invoice ? (
-                      <QRCodeSVG
-                        value={invoice}
-                        size={300}
-                        imageSettings={{
-                          src: 'https://placehold.co/42x42',
-                          x: undefined,
-                          y: undefined,
-                          height: 42,
-                          width: 42,
-                          excavate: true,
-                        }}
-                      />
-                    ) : (
-                      <Skeleton className='w-[300px] h-[300px]' />
-                    )}
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <div className='flex gap-2 w-full'>
-                    <Button variant='secondary' className='w-full' onClick={() => null}>
-                      Pay with wallet
-                    </Button>
-                    <Button className='w-full' onClick={() => setScreen('summary')}>
-                      Copy
-                    </Button>
-                  </div>
-                </CardFooter>
+              <Card className='py-8 md:py-12 bg-white'>
+                <div className='max-w-[300px] mx-auto'>
+                  {invoice ? (
+                    <QRCodeSVG
+                      value={invoice}
+                      size={300}
+                      imageSettings={{
+                        // Iso 24x24, image 42x42
+                        src: '/iso.png',
+                        x: undefined,
+                        y: undefined,
+                        height: 42,
+                        width: 42,
+                        excavate: true,
+                      }}
+                    />
+                  ) : (
+                    <Skeleton className='w-[300px] h-[300px]' />
+                  )}
+                </div>
               </Card>
+              <div className='flex gap-2 w-full mt-2'>
+                <Button variant='secondary' className='w-full' onClick={() => null}>
+                  Pay with wallet
+                </Button>
+                <Button className='w-full' onClick={() => null}>
+                  Copy
+                </Button>
+              </div>
             </TabsContent>
             <TabsContent value='onchain'>
               <Card>
@@ -119,7 +74,7 @@ export function FormPayment({ invoice }: any) {
                 </CardContent>
                 <CardFooter>
                   <div className='flex gap-2 w-full justify-center'>
-                    <p className='text-gray-500 text-sm'>Service not available.</p>
+                    <p className='text-text text-sm'>Service not available.</p>
                   </div>
                 </CardFooter>
               </Card>
@@ -128,5 +83,28 @@ export function FormPayment({ invoice }: any) {
         </div>
       </div>
     </>
+  );
+}
+
+function TimeIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      width='20'
+      height='20'
+      color='currentColor'
+      fill='none'
+    >
+      <circle cx='12' cy='12' r='10' stroke='currentColor' stroke-width='1.5' />
+      <path
+        d='M12 8V12L14 14'
+        stroke='currentColor'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
   );
 }
