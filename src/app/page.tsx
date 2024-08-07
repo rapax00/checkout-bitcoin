03c67ea047513199e51d53b99816845b7bfbfd73
Agustin Kassis <webmaster@masize.com>
2024-08-07 18:58:19 -0300

    added mock implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { LaWalletConfig, useZap } from '@lawallet/react';
+import { useState, useEffect, useCallback } from "react";
+import Link from "next/link";
+import { LaWalletConfig, useZap } from "@lawallet/react";
+import { Event } from "nostr-tools";
 
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Navbar } from '@/components/navbar';
+} from "@/components/ui/breadcrumb";
+import { Navbar } from "@/components/navbar";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -24,13 +25,21 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+} from "@/components/ui/alert-dialog";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-import { cn } from '@/lib/utils';
-import { config } from '@/config/config';
+import { cn } from "@/lib/utils";
+import { config } from "@/config/config";
 
 // Generic
+import { FormCustomer } from "./component/form-customer";
+import { FormPayment } from "./component/form-payment";
+import { Order, OrderRequest, OrderUserData } from "./types/orders";
 
 // Icons
 import { SleepingIcon } from "@/components/icons/SleepingIcon";
@@ -38,59 +47,96 @@ import { CreditCardValidationIcon } from "@/components/icons/CreditCardValidatio
 import { PlusIcon } from "@/components/icons/PlusIcon";
 import { MinusIcon } from "@/components/icons/MinusIcon";
 
+import useOrder from "./hooks/useOrder";
+
+// Mock data
+const TICKET = {
+  title: "Ticket #1",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit impedit aperiam, optio dolores tenetur earum.",
+  imageUrl: "https://placehold.co/400",
+  value: 1,
+  valueType: "SAT",
+};
+
+const ZAP_RECEIPT_EVENT: Event = {
+  content: "",
+  created_at: 123123,
+  id: "",
+  kind: 9735,
+  pubkey: "",
+  sig: "",
+  tags: [],
+};
+
 export default function Page() {
-  const [countTickets, setCountTickets] = useState<number>(1);
-  const [screen, setScreen] = useState<string>('information');
+  // Flow
+  const [screen, setScreen] = useState<string>("information");
+  const [isLoading, setIsloading] = useState<boolean>(false);
 
   // Dialog for reset invoice
-  const [open, setOpen] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(false);
 
-  // Mock data
-  const ticket = {
-    title: 'Ticket #1',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit impedit aperiam, optio dolores tenetur earum.',
-    imageUrl: 'https://placehold.co/400',
-    value: 1,
-    valueType: 'SAT',
-  };
+  // Hooks
+  const { requestNewOrder, claimOrderPayment } = useOrder();
 
-  // yisus@lawallet.ar
-  const userPubkey: string = 'aed7a26265c4c9dad6a1c6f185a0cc2e6e638c9cc8666af020ea06dfe620937a';
+  // Data
+  const [countTickets, setCountTickets] = useState<number>(1);
+  const [invoice, setInvoice] = useState<string>();
+  const [orderReferenceId, setOrderReferenceId] = useState<string>();
+  const [isPaid, setIsPaid] = useState<boolean>(false);
 
-  const { invoice, createZapInvoice, resetInvoice } = useZap({
-    receiverPubkey: userPubkey,
-    config,
-  });
+  const clear = useCallback(() => {
+    setOrderReferenceId(undefined);
+    setInvoice(undefined);
 
-  const handleGenerateInvoice = async () => {
-    if (invoice.loading) return;
+    setIsPaid(false);
+  }, []);
 
-    // Set value on SAT
-    const localValue = ticket.value * countTickets;
+  const emulateZapPayment = useCallback(async () => {
+    const order = await claimOrderPayment(ZAP_RECEIPT_EVENT);
+    console.info("order:");
+    console.dir(order);
+    setIsPaid(true);
+  }, [claimOrderPayment]);
 
-    createZapInvoice(localValue).then((bolt11: string | undefined) => {
-      if (!bolt11) {
-        console.log('upds, algo paso mal');
-        return;
+  const handleCreateOrder = useCallback(
+    async (data: OrderUserData) => {
+      if (isLoading) return;
+
+      setIsloading(true);
+      clear();
+
+      // Create new order
+      try {
+        const order = await requestNewOrder({ ...data, qty: countTickets });
+        setInvoice(order.pr);
+        setOrderReferenceId(order.orderReferenceId);
+
+        setScreen("payment");
+        window.scrollTo({
+          top: 0,
+          behavior: "auto",
+        });
+
+        // Emulate payment
+        setTimeout(() => {
+          //emulateZapPayment();
+        }, 1000);
+      } catch {
+        alert("Error creating order");
+      } finally {
+        setIsloading(false);
       }
-
-      setScreen('payment');
-      window.scrollTo({
-        top: 0,
-        behavior: 'auto',
-      });
-    });
-  };
-
-  const handleSaveTicket = () => {};
+    },
+    [isLoading, countTickets, clear, requestNewOrder, emulateZapPayment]
+  );
 
   useEffect(() => {
-    if (invoice.payed) {
-      handleSaveTicket();
-      setScreen('summary');
+    if (isPaid) {
+      setScreen("summary");
     }
-  }, [invoice.payed]);
+  }, [isPaid]);
 
   return (
     <LaWalletConfig config={config}>
@@ -98,61 +144,82 @@ export default function Page() {
         {/* Aside info */}
         <aside className='bg-card relative flex justify-center items-center w-full min-h-full pt-[60px] md:pt-0'>
           <Navbar />
-          <div className={cn('w-full max-w-[520px]  px-4', screen === 'information' ? 'my-4' : '')}>
-            {screen === 'information' ? (
+          <div
+            className={cn(
+              "w-full max-w-[520px]  px-4",
+              screen === "information" ? "my-4" : ""
+            )}
+          >
+            {screen === "information" ? (
               <>
                 <Card className='p-4 bg-background'>
                   <div className='flex justify-between items-center gap-4'>
                     <div>
-                      <h2 className='text-md'>{ticket.title}</h2>
+                      <h2 className='text-md'>{TICKET.title}</h2>
                       <p className='font-semibold text-lg'>
-                        {ticket.value} {ticket.valueType}
+                        {TICKET.value} {TICKET.valueType}
                       </p>
                     </div>
                     <div className='flex gap-2 items-center'>
                       <Button
-                        variant={screen !== 'information' || countTickets === 1 ? 'ghost' : 'secondary'}
+                        variant={
+                          screen !== "information" || countTickets === 1
+                            ? "ghost"
+                            : "secondary"
+                        }
                         size='icon'
                         onClick={() => setCountTickets(countTickets - 1)}
-                        disabled={screen !== 'information' || countTickets === 1}
+                        disabled={
+                          screen !== "information" || countTickets === 1
+                        }
                       >
                         <MinusIcon />
                       </Button>
                       <p className='flex items-center justify-center gap-1 w-[40px] font-semibold'>
-                        {screen !== 'information' && <span className='font-normal text-xs text-text'>x</span>}
+                        {screen !== "information" && (
+                          <span className='font-normal text-xs text-text'>
+                            x
+                          </span>
+                        )}
                         {countTickets}
                       </p>
                       <Button
-                        variant={screen !== 'information' ? 'ghost' : 'secondary'}
+                        variant={
+                          screen !== "information" ? "ghost" : "secondary"
+                        }
                         size='icon'
                         onClick={() => setCountTickets(countTickets + 1)}
-                        disabled={screen !== 'information'}
+                        disabled={screen !== "information"}
                       >
                         <PlusIcon />
                       </Button>
                     </div>
                   </div>
-                  <p className='mt-2 text-sm text-text'>{ticket.description}</p>
+                  <p className='mt-2 text-sm text-text'>{TICKET.description}</p>
                 </Card>
 
                 <div className='p-4'>
                   <div className='flex gap-4 justify-between items-center'>
                     <p className='text-text'>Total</p>
                     <p className='font-bold text-md'>
-                      {ticket.value * countTickets} {ticket.valueType}
+                      {TICKET.value * countTickets} {TICKET.valueType}
                     </p>
                   </div>
                 </div>
               </>
             ) : (
               <>
-                <Accordion type='single' collapsible className='w-full md:hidden'>
+                <Accordion
+                  type='single'
+                  collapsible
+                  className='w-full md:hidden'
+                >
                   <AccordionItem value='item-1'>
                     <AccordionTrigger className='flex gap-2 no-underline'>
                       <div className='flex items-center justify-between gap-2 w-full'>
                         Show order summary
                         <p className='font-bold text-lg no-underline'>
-                          {ticket.value * countTickets} {ticket.valueType}
+                          {TICKET.value * countTickets} {TICKET.valueType}
                         </p>
                       </div>
                     </AccordionTrigger>
@@ -160,14 +227,16 @@ export default function Page() {
                       <Card className='p-4 bg-background'>
                         <div className='flex justify-between items-center gap-4'>
                           <div>
-                            <h2 className='text-md'>{ticket.title}</h2>
+                            <h2 className='text-md'>{TICKET.title}</h2>
                             <p className='font-semibold text-lg'>
-                              {ticket.value} {ticket.valueType}
+                              {TICKET.value} {TICKET.valueType}
                             </p>
                           </div>
                           <div className='flex gap-2 items-center'>
                             <p className='flex items-center justify-center gap-1 w-[40px] font-semibold'>
-                              {screen !== 'information' && <span className='font-normal text-text'>x</span>}
+                              {screen !== "information" && (
+                                <span className='font-normal text-text'>x</span>
+                              )}
                               {countTickets}
                             </p>
                           </div>
@@ -177,7 +246,7 @@ export default function Page() {
                         <div className='flex gap-4 justify-between items-center'>
                           <p className='text-text text-md'>Total</p>
                           <p className='font-bold text-md'>
-                            {ticket.value * countTickets} {ticket.valueType}
+                            {TICKET.value * countTickets} {TICKET.valueType}
                           </p>
                         </div>
                       </div>
@@ -189,14 +258,16 @@ export default function Page() {
                   <Card className='p-4 bg-background'>
                     <div className='flex justify-between items-center gap-4'>
                       <div>
-                        <h2 className='text-md'>{ticket.title}</h2>
+                        <h2 className='text-md'>{TICKET.title}</h2>
                         <p className='font-semibold text-lg'>
-                          {ticket.value} {ticket.valueType}
+                          {TICKET.value} {TICKET.valueType}
                         </p>
                       </div>
                       <div className='flex gap-2 items-center'>
                         <p className='flex items-center justify-center gap-1 w-[40px] font-semibold'>
-                          {screen !== 'information' && <span className='font-normal text-text'>x</span>}
+                          {screen !== "information" && (
+                            <span className='font-normal text-text'>x</span>
+                          )}
                           {countTickets}
                         </p>
                       </div>
@@ -206,7 +277,7 @@ export default function Page() {
                     <div className='flex gap-4 justify-between items-center'>
                       <p className='text-text'>Total</p>
                       <p className='font-bold text-md'>
-                        {ticket.value * countTickets} {ticket.valueType}
+                        {TICKET.value * countTickets} {TICKET.valueType}
                       </p>
                     </div>
                   </div>
@@ -224,19 +295,34 @@ export default function Page() {
                 <Breadcrumb>
                   <BreadcrumbList>
                     <BreadcrumbItem>
-                      <BreadcrumbPage className={cn('', screen === 'information' ? 'text-white' : 'text-text')}>
+                      <BreadcrumbPage
+                        className={cn(
+                          "",
+                          screen === "information" ? "text-white" : "text-text"
+                        )}
+                      >
                         Information
                       </BreadcrumbPage>
                     </BreadcrumbItem>
                     <BreadcrumbSeparator />
                     <BreadcrumbItem>
-                      <BreadcrumbPage className={cn('', screen === 'payment' ? 'text-white' : 'text-text')}>
+                      <BreadcrumbPage
+                        className={cn(
+                          "",
+                          screen === "payment" ? "text-white" : "text-text"
+                        )}
+                      >
                         Payment
                       </BreadcrumbPage>
                     </BreadcrumbItem>
                     <BreadcrumbSeparator />
                     <BreadcrumbItem>
-                      <BreadcrumbPage className={cn('', screen === 'summary' ? 'text-white' : 'text-text')}>
+                      <BreadcrumbPage
+                        className={cn(
+                          "",
+                          screen === "summary" ? "text-white" : "text-text"
+                        )}
+                      >
                         Summary
                       </BreadcrumbPage>
                     </BreadcrumbItem>
@@ -245,11 +331,13 @@ export default function Page() {
               </div>
             </div>
 
-            {screen === 'information' && <FormCustomer onSubmit={handleGenerateInvoice} />}
+            {screen === "information" && (
+              <FormCustomer onSubmit={handleCreateOrder} />
+            )}
 
-            {screen === 'payment' && <FormPayment invoice={invoice?.bolt11.toUpperCase()} />}
+            {screen === "payment" && <FormPayment invoice={invoice} />}
 
-            {screen === 'summary' && (
+            {screen === "summary" && (
               <>
                 <Card>
                   <div className='flex-1 flex flex-col items-center justify-center gap-4 w-full mx-auto py-12 px-8'>
@@ -257,7 +345,8 @@ export default function Page() {
                     <div className='flex flex-col gap-2 text-center'>
                       <h2 className='font-bold text-2xl'>Congratulation!</h2>
                       <p className='text-text'>
-                        Your payment has been confirmed. We have sent the event details to your email.
+                        Your payment has been confirmed. We have sent the event
+                        details to your email.
                       </p>
                     </div>
                   </div>
@@ -277,7 +366,9 @@ export default function Page() {
         <AlertDialogContent>
           <AlertDialogHeader className='items-center'>
             <SleepingIcon className='w-8 h-8 color-primary' />
-            <AlertDialogTitle className='text-center'>Oops! Try again</AlertDialogTitle>
+            <AlertDialogTitle className='text-center'>
+              Oops! Try again
+            </AlertDialogTitle>
             <AlertDialogDescription className='text-center'>
               {`It looks like you weren't able to complete the transaction in time.`}
             </AlertDialogDescription>
