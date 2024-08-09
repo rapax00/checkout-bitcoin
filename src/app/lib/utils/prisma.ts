f1f48665027a0745cf1f75255aad0d88eb4b493a
Rapax <gasparpelayo44@gmail.com>
2024-08-09 17:15:39 -0300

    Add updateOrder

--- src/app/lib/utils/prisma.ts
+++ src/app/lib/utils/prisma.ts
@@ -2,10 +2,19 @@ import { prisma } from "@/app/services/prismaClient";
 import { Order, User } from "@prisma/client";
 import { randomBytes } from "crypto";
 import { calculateTicketPrice } from "./price";
+import { Event } from "nostr-tools";
 
 export interface CreateOrderResponse {
-  totalSats: number;
+  totalMiliSats: number;
   referenceId: string;
+}
+
+export interface updateOrderResponse {
+  referenceId: string;
+  qty: number;
+  totalMiliSats: number;
+  zapReceiptId: string;
+  paid: boolean;
 }
 
 export async function createOrder(
@@ -15,7 +24,6 @@ export async function createOrder(
 ): Promise<CreateOrderResponse> {
   let user: User = await prisma.user.upsert({
     where: {
-      fullname,
       email,
     },
     update: {},
@@ -23,22 +31,63 @@ export async function createOrder(
   });
 
   const referenceId: string = randomBytes(32).toString("hex");
-  // const ticketPriceArs: number = parseInt(process.env.TICKET_PRICE_ARS!);
-  //   const orderTotalSats = await calculateTicketPrice(qty, ticketPriceArs);
-  const totalSats = 1000;
+  const ticketPriceArs: number = parseInt(process.env.TICKET_PRICE_ARS!);
+  const totalMiliSats = await calculateTicketPrice(qty, ticketPriceArs);
 
   const order: Order = await prisma.order.create({
     data: {
       referenceId,
       qty,
-      totalSats,
+      totalMiliSats,
       userId: user.id,
     },
   });
 
   const response: CreateOrderResponse = {
-    totalSats,
+    totalMiliSats,
     referenceId,
+  };
+
+  return response;
+}
+
+export async function updateOrder(
+  fullname: string,
+  email: string,
+  zapReceipt: Event
+): Promise<updateOrderResponse> {
+  const orderReferenceId = zapReceipt.tags.find((tag) => tag[0] === "e")![1];
+
+  // Update order to paid
+  const order: Order | null = await prisma.order.update({
+    where: {
+      referenceId: orderReferenceId,
+    },
+    data: {
+      paid: true,
+      zapReceiptId: zapReceipt.id,
+    },
+  });
+
+  const user: User | null = await prisma.user.update({
+    where: {
+      email: email,
+    },
+    data: {
+      fullname,
+    },
+  });
+
+  if (!order || !user) {
+    throw new Error("Order or user not found");
+  }
+
+  const response: updateOrderResponse = {
+    referenceId: orderReferenceId,
+    qty: order.qty,
+    totalMiliSats: order.totalMiliSats,
+    zapReceiptId: order.zapReceiptId!,
+    paid: order.paid,
   };
 
   return response;
