3daa52cd496a6debed8d51d567f3f2fe24ed7261
Rapax <gasparpelayo44@gmail.com>
2024-08-09 17:15:10 -0300

    Minor changes

--- src/app/services/ln.ts
+++ src/app/services/ln.ts
@@ -1,28 +1,30 @@
-import { EventTemplate } from "nostr-tools";
+import { Event } from "nostr-tools";
 
 async function generateInvoice(
   callbackUrl: string,
   amount: number,
-  zapEvent?: EventTemplate
+  zapEvent?: Event
 ): Promise<string> {
   let url = `${callbackUrl}?amount=${amount}`;
 
   if (zapEvent) {
     const encodedZapEvent = encodeURI(JSON.stringify(zapEvent));
 
-    url += `&nostr=${encodedZapEvent}&lnurl=1`;
+    url += `&nostr=${encodedZapEvent}&lnurl=lnurl`;
   }
+
+  console.log("url", url, "\n"); // debug
 
   return ((await (await fetch(url)).json()) as any).pr as string;
 }
 
-async function getCallbackUrlFromWalias(walias: string) {
+async function getLnurlpFromWalias(walias: string): Promise<any> {
   const name = walias.split("@")[0];
   const domain = walias.split("@")[1];
 
   const url = `https://${domain}/.well-known/lnurlp/${name}`;
 
-  return (await (await fetch(url)).json()).callback as string;
+  return await (await fetch(url)).json();
 }
 
-export { generateInvoice, getCallbackUrlFromWalias };
+export { generateInvoice, getLnurlpFromWalias };
