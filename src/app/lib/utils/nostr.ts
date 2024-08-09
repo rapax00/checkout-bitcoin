811c433c498c9a665d1074a6f4098ed73df7310a
Rapax <gasparpelayo44@gmail.com>
2024-08-09 17:16:17 -0300

    Move to utils

---
+++ src/app/lib/utils/nostr.ts
@@ -0,0 +1,35 @@
+import { Event, EventTemplate, finalizeEvent, getPublicKey } from "nostr-tools";
+
+function generateZapRequest(
+  orderId: string,
+  amountSats: number,
+  nostrPubkey: string
+): Event {
+  const publicKey = getPublicKey(
+    Uint8Array.from(Buffer.from(process.env.SIGNER_PRIVATE_KEY!, "hex"))
+  ); // debug
+  console.log("publicKey", publicKey, "\n"); // debug
+
+  const unsignedZapRequest: EventTemplate = {
+    kind: 9734,
+    tags: [
+      ["p", nostrPubkey],
+      ["amount", amountSats.toString()],
+      ["relays", "wss://relay.lawallet.ar", "wss://nostr-pub.wellorder.net"],
+      ["e", orderId],
+    ] as string[][],
+    content: "",
+    created_at: Math.round(Date.now() / 1000),
+  };
+
+  const privateKey = Uint8Array.from(
+    Buffer.from(process.env.SIGNER_PRIVATE_KEY!, "hex")
+  );
+  const zapRequest: Event = finalizeEvent(unsignedZapRequest, privateKey);
+
+  console.log("zapRequest", zapRequest, "\n"); // debug
+
+  return zapRequest;
+}
+
+export { generateZapRequest };
