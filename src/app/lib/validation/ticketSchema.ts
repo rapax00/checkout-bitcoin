388a993f74b5c28fc4dc05c6106ddbc9ec44dda6
Rapax <gasparpelayo44@gmail.com>
2024-08-12 23:39:44 -0300

    Add isSubscribed parameter

--- src/app/lib/validation/ticketSchema.ts
+++ src/app/lib/validation/ticketSchema.ts
@@ -4,4 +4,5 @@ export const ticketSchema = z.object({
   fullname: z.string().min(3, { message: "Fullname is required" }),
   email: z.string().email({ message: "Invalid email address" }),
   qty: z.number().int().lt(1000).positive({ message: "Qty Must be a number" }),
+  isSubscribed: z.boolean({ message: "isSubscribed must be a boolean" }),
 });
