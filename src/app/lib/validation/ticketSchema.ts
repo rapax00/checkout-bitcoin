651a7a97cc898bdc16e9bd6d074b2871459ea475
Rapax <gasparpelayo44@gmail.com>
2024-08-14 15:57:13 -0300

    Rename newsletter

--- src/app/lib/validation/ticketSchema.ts
+++ src/app/lib/validation/ticketSchema.ts
@@ -4,5 +4,5 @@ export const ticketSchema = z.object({
   fullname: z.string().min(3, { message: "Fullname is required" }),
   email: z.string().email({ message: "Invalid email address" }),
   qty: z.number().int().lt(1000).positive({ message: "Qty Must be a number" }),
-  isSubscribed: z.boolean({ message: "isSubscribed must be a boolean" }),
+  newsletter: z.boolean({ message: "Newsletter must be a boolean" }),
 });
