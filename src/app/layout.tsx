48347638f5ab99d34f277d4f84f14f9e98ff23be
Rapax <gasparpelayo44@gmail.com>
2024-08-15 16:19:31 -0300

    Add AppWrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 
 import './globals.css';
+import { LaWalletConfig } from '@lawallet/react';
+import AppWrapper from '@/components/wrapper';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -18,8 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className={cn('min-h-screen font-sans antialiased', inter.variable)}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn('min-h-screen font-sans antialiased', inter.variable)}
+      >
+        <AppWrapper>{children}</AppWrapper>
+      </body>
     </html>
   );
 }
