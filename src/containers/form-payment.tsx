586f5138b592acb6f2b4386a7221cc21ce18ac25
Rapax <gasparpelayo44@gmail.com>
2024-08-15 16:18:36 -0300

    Fix copy button

--- src/containers/form-payment.tsx
+++ src/containers/form-payment.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { QRCodeSVG } from "qrcode.react";
+import { QRCodeSVG } from 'qrcode.react';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -10,47 +10,55 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Badge } from "@/components/ui/badge";
+} from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Badge } from '@/components/ui/badge';
 
 interface FormPaymentProps {
   invoice?: string;
 }
 
 export function FormPayment({ invoice }: FormPaymentProps) {
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(invoice!);
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  };
+
   return (
     <>
-      <div className='flex-1 flex flex-col gap-4'>
-        <div className='flex gap-4 items-center justify-end'>
-          <p className='text-sm text-text'>Time:</p>
-          <div className='flex gap-2 items-center justify-center'>
-            <TimeIcon className='w-4 h-4' />
-            <span className='font-semibold text-sm'>09:45</span>
+      <div className="flex-1 flex flex-col gap-4">
+        <div className="flex gap-4 items-center justify-end">
+          <p className="text-sm text-text">Time:</p>
+          <div className="flex gap-2 items-center justify-center">
+            <TimeIcon className="w-4 h-4" />
+            <span className="font-semibold text-sm">09:45</span>
           </div>
         </div>
 
-        <div className='flex-1 flex flex-col gap-4 justify-start'>
-          <Tabs defaultValue='lightning'>
-            <TabsList className='w-full bg-border'>
-              <TabsTrigger value='lightning' className='flex-1'>
+        <div className="flex-1 flex flex-col gap-4 justify-start">
+          <Tabs defaultValue="lightning">
+            <TabsList className="w-full bg-border">
+              <TabsTrigger value="lightning" className="flex-1">
                 Lightning
               </TabsTrigger>
-              <TabsTrigger value='onchain' className='flex-1 gap-2' disabled>
+              <TabsTrigger value="onchain" className="flex-1 gap-2" disabled>
                 Onchain <Badge>Soon</Badge>
               </TabsTrigger>
             </TabsList>
-            <TabsContent value='lightning'>
-              <Card className='py-8 md:py-12 bg-white'>
-                <div className='max-w-[300px] mx-auto'>
+            <TabsContent value="lightning">
+              <Card className="py-8 md:py-12 bg-white">
+                <div className="max-w-[300px] mx-auto">
                   {invoice ? (
                     <QRCodeSVG
                       value={invoice}
                       size={300}
                       imageSettings={{
                         // Iso 24x24, image 42x42
-                        src: "/iso.png",
+                        src: '/iso.png',
                         x: undefined,
                         y: undefined,
                         height: 42,
@@ -59,24 +67,24 @@ export function FormPayment({ invoice }: FormPaymentProps) {
                       }}
                     />
                   ) : (
-                    <Skeleton className='w-[300px] h-[300px]' />
+                    <Skeleton className="w-[300px] h-[300px]" />
                   )}
                 </div>
               </Card>
-              <div className='flex gap-2 w-full mt-2'>
+              <div className="flex gap-2 w-full mt-2">
                 <Button
-                  variant='secondary'
-                  className='w-full'
+                  variant="secondary"
+                  className="w-full"
                   onClick={() => null}
                 >
                   Pay with wallet
                 </Button>
-                <Button className='w-full' onClick={() => null}>
+                <Button className="w-full" onClick={handleCopy}>
                   Copy
                 </Button>
               </div>
             </TabsContent>
-            <TabsContent value='onchain'>
+            <TabsContent value="onchain">
               <Card>
                 <CardHeader>
                   <CardTitle>Bitcoin Onchain</CardTitle>
@@ -85,13 +93,13 @@ export function FormPayment({ invoice }: FormPaymentProps) {
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <div className='max-w-[300px] mx-auto'>
-                    <Skeleton className='w-[300px] h-[300px]' />
+                  <div className="max-w-[300px] mx-auto">
+                    <Skeleton className="w-[300px] h-[300px]" />
                   </div>
                 </CardContent>
                 <CardFooter>
-                  <div className='flex gap-2 w-full justify-center'>
-                    <p className='text-text text-sm'>Service not available.</p>
+                  <div className="flex gap-2 w-full justify-center">
+                    <p className="text-text text-sm">Service not available.</p>
                   </div>
                 </CardFooter>
               </Card>
@@ -107,20 +115,20 @@ function TimeIcon(props: any) {
   return (
     <svg
       {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 24 24'
-      width='20'
-      height='20'
-      color='currentColor'
-      fill='none'
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="20"
+      height="20"
+      color="currentColor"
+      fill="none"
     >
-      <circle cx='12' cy='12' r='10' stroke='currentColor' stroke-width='1.5' />
+      <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5" />
       <path
-        d='M12 8V12L14 14'
-        stroke='currentColor'
-        stroke-width='1.5'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        d="M12 8V12L14 14"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
