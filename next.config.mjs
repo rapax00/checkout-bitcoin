9a7cf8d5d711987eed4693b111a0e123d9aa030a
Rapax <gasparpelayo44@gmail.com>
2024-08-19 17:37:37 -0300

    fix: add others env

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABE_URL,
     SUPABASE_SECRET: process.env.SUPABASE_API_TOKEN,
     SIGNER_KEY: process.env.SIGNER_PRIVATE_KEY,
+    TICKET_PRICE: process.env.TICKET_PRICE_ARS,
+    SENDY_URL: process.env.SENDY_API_URL,
+    SENDY_KEY: process.env.SENDY_API_KEY,
+    SENDY_LIST: process.env.SENDY_LIST_ID,
   },
   images: {
     remotePatterns: [
