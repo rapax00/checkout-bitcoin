0757f60dc367944d5a8f509a191ebdf25386dea7
Rapax <gasparpelayo44@gmail.com>
2024-09-09 22:37:40 -0300

    fix: remove unused

--- src/config/db.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Configura tu cliente de Supabase
-const url = process.env.SUPABASE_URL || '';
-const key = process.env.SUPABASE_SECRET || '';
-
-export const supabase = createClient(url, key);
