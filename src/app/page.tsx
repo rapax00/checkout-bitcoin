b8070b228d6056ab0b8387deec8d00084a0c293f
Rapax <gasparpelayo44@gmail.com>
2024-10-14 17:57:59 -0300

    fix: back to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default function Page() {
   );
 
   // Nostr
-  const { events, relay } = useRelay({
+  const { events, relay, clearEvents } = useRelay({
     relayUrl: 'wss://relay.lawallet.ar',
     filters,
     closeOnEose: false,
@@ -263,7 +263,17 @@ export default function Page() {
     setPaymentRequest(undefined);
     setVerifyUrl(undefined);
     setCode('');
-  }, [setEventReferenceId, setTicketQuantity, setPaymentRequest, setCode]);
+    setUserData(undefined);
+    clear();
+    clearEvents();
+  }, [
+    setEventReferenceId,
+    setTicketQuantity,
+    setPaymentRequest,
+    setCode,
+    clear,
+    clearEvents,
+  ]);
 
   // Update ticket price calculations
   useEffect(() => {
